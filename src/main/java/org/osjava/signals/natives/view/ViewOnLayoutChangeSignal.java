4879de75ec55e9c4b227c007ebc25a1fba81280c
Simon Richardson <me@simonrichardson.info>
2011-11-14 18:33:39 +0000

    Adding changes to the onlayout change signal, so we can target API 11 from API 7

--- src/main/java/org/osjava/signals/natives/view/ViewOnLayoutChangeSignal.java
+++ src/main/java/org/osjava/signals/natives/view/ViewOnLayoutChangeSignal.java
@@ -1,20 +1,32 @@
 package org.osjava.signals.natives.view;
 
-import java.awt.Rectangle;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
-import org.osjava.signals.impl.NativeSignalImpl.NativeSignalImpl3;
+import org.osjava.signals.impl.NativeSignalImpl.NativeSignalImpl5;
 
 import android.view.View;
 
-public class ViewOnLayoutChangeSignal extends NativeSignalImpl3<View, Rectangle, Rectangle> {
+public class ViewOnLayoutChangeSignal extends
+		NativeSignalImpl5<View, Integer, Integer, Integer, Integer> {
 
-	private final static String CLASS_METHOD_NAME = "setOnLayoutChangeListener";
+	private final static String CLASS_NAME = "android.view.View$OnLayoutChangeListener";
 
-	private final TargetListener _listener = new TargetListener();
+	private final static String CLASS_SUBSCRIBER_NAME = "addOnLayoutChangeListener";
+
+	private final static String CLASS_LISTENER_NAME = "onLayoutChange";
+
+	private TargetListener _listener = new TargetListener();
 
 	private Method _method;
+
+	private Method _delegateMethod;
+
+	private Class<?> _interface;
+
+	private Object _proxy;
 
 	/**
 	 * Private constructor
@@ -23,6 +35,7 @@ public class ViewOnLayoutChangeSignal extends NativeSignalImpl3<View, Rectangle,
 	 *            to be used when applying the listeners
 	 */
 	private ViewOnLayoutChangeSignal(View target) {
+		init();
 		setTarget(target);
 	}
 
@@ -44,7 +57,36 @@ public class ViewOnLayoutChangeSignal extends NativeSignalImpl3<View, Rectangle,
 	 */
 	@Override
 	public boolean isAvailable() {
-		return null != _method;
+		return null != _interface && null != _delegateMethod && null != _proxy && null != _method;
+	}
+
+	/**
+	 * Initialise method for the signal
+	 */
+	protected void init() {
+		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+		try {
+			_interface = classLoader.loadClass(CLASS_NAME);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		final Class<?>[] parameterTypes = { View.class, Integer.class, Integer.class,
+				Integer.class, Integer.class, Integer.class, Integer.class, Integer.class,
+				Integer.class };
+
+		try {
+			_delegateMethod = _listener.getClass().getMethod(CLASS_LISTENER_NAME, parameterTypes);
+		} catch (SecurityException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+
+		final Class<?>[] proxyInterfaces = { _interface };
+		_proxy = Proxy.newProxyInstance(classLoader, proxyInterfaces, new TargetProxyListener(
+				_listener, _delegateMethod));
 	}
 
 	/**
@@ -58,7 +100,8 @@ public class ViewOnLayoutChangeSignal extends NativeSignalImpl3<View, Rectangle,
 				if (_method.isAccessible())
 					_method.setAccessible(true);
 
-				_method.invoke(view);
+				Object[] arguments = { null };
+				_method.invoke(view, arguments);
 			} catch (IllegalArgumentException e) {
 				throw new RuntimeException(e);
 			} catch (IllegalAccessException e) {
@@ -74,37 +117,36 @@ public class ViewOnLayoutChangeSignal extends NativeSignalImpl3<View, Rectangle,
 	 */
 	@Override
 	protected void registerTargetListener() {
-		View view = getTarget();
-		if (null != view) {
-			if (null == _method) {
-				try {
-					final Method[] methods = view.getClass().getMethods();
-					for (final Method method : methods) {
-						if (method.getName().equals(CLASS_METHOD_NAME)) {
-							// We know the contact so we can skip the method
-							// name
-							_method = method;
-							break;
+		if (null != _interface && null != _proxy) {
+			View view = getTarget();
+			if (null != view) {
+				if (null == _method) {
+					try {
+						final Method[] methods = view.getClass().getMethods();
+						for (final Method method : methods) {
+							if (method.getName().equals(CLASS_SUBSCRIBER_NAME)) {
+								// We know the contact so we can skip the method
+								// name
+								_method = method;
+								break;
+							}
 						}
+					} catch (SecurityException e) {
+						throw new RuntimeException(e);
 					}
-				} catch (SecurityException e) {
-					throw new RuntimeException(e);
 				}
-			}
 
-			// If we've got the method invoke on it
-			if (null != _method) {
-				try {
-					if (_method.isAccessible())
-						_method.setAccessible(true);
-
-					_method.invoke(view, _listener);
-				} catch (IllegalArgumentException e) {
-					throw new RuntimeException(e);
-				} catch (IllegalAccessException e) {
-					throw new RuntimeException(e);
-				} catch (InvocationTargetException e) {
-					throw new RuntimeException(e);
+				if (null != _method) {
+					try {
+						Object[] arguments = { _proxy };
+						_method.invoke(view, arguments);
+					} catch (IllegalArgumentException e) {
+						throw new RuntimeException(e);
+					} catch (IllegalAccessException e) {
+						throw new RuntimeException(e);
+					} catch (InvocationTargetException e) {
+						throw new RuntimeException(e);
+					}
 				}
 			}
 		}
@@ -113,15 +155,47 @@ public class ViewOnLayoutChangeSignal extends NativeSignalImpl3<View, Rectangle,
 	/**
 	 * Private target listener class which implements the
 	 */
+	private final class TargetProxyListener implements InvocationHandler {
+
+		private final TargetListener _delegate;
+
+		private final Method _delegateMethod;
+
+		public TargetProxyListener(TargetListener delegate, Method delegateMethod) {
+			assert null != delegate : "TargetListener can not be null";
+			assert null != delegateMethod : "DelegateMethod can not be null";
+
+			_delegate = delegate;
+			_delegateMethod = delegateMethod;
+		}
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
+			if (null != _delegateMethod) {
+				if (method.getName().equals(CLASS_LISTENER_NAME)) {
+					try {
+						if (!_delegateMethod.isAccessible())
+							_delegateMethod.setAccessible(true);
+
+						return _delegateMethod.invoke(_delegate, arguments);
+					} catch (InvocationTargetException e) {
+						throw e.getTargetException();
+					}
+				}
+			}
+
+			throw new InternalError("Unexpected method dispatched: " + method);
+		}
+	}
+
 	private final class TargetListener {
 
 		@SuppressWarnings("unused")
-		public void onLayoutChange(final View v, final int left, final int top, final int right,
-				final int bottom, final int oldLeft, final int oldTop, final int oldRight,
-				final int oldBottom) {
+		public void onLayoutChange(View view, Integer left, Integer top, Integer right,
+				Integer bottom, Integer oldLeft, Integer oldTop, Integer oldRight, Integer oldBottom) {
+
 			try {
-				dispatch(v, new Rectangle(left, top, right, bottom), new Rectangle(oldLeft, oldTop,
-						oldRight, oldBottom));
+				ViewOnLayoutChangeSignal.this.dispatch(view, left, top, right, bottom);
 			} catch (Throwable t) {
 				throw new RuntimeException(t);
 			}
