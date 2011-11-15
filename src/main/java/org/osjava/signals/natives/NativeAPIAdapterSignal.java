bf785022751b01499021c06573ea5ee2348394e4
Simon Richardson <stickupkid@gmail.comm>
2011-11-15 10:49:12 +0000

    Implementing a native api adapter signal

---
+++ src/main/java/org/osjava/signals/natives/NativeAPIAdapterSignal.java
@@ -0,0 +1,707 @@
+package org.osjava.signals.natives;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.osjava.signals.Dispatcher;
+import org.osjava.signals.SignalListener;
+import org.osjava.signals.SignalListener.SignalListener1;
+import org.osjava.signals.SignalListener.SignalListener2;
+import org.osjava.signals.SignalListener.SignalListener3;
+import org.osjava.signals.SignalListener.SignalListener4;
+import org.osjava.signals.SignalListener.SignalListener5;
+import org.osjava.signals.Slot;
+import org.osjava.signals.impl.DispatcherImpl;
+import org.osjava.signals.impl.NativeSignalImpl;
+
+public class NativeAPIAdapterSignal<L extends SignalListener, T> extends NativeSignalImpl<L, T> {
+
+	private Method _invokeMethod;
+
+	private String _listenerName;
+
+	private Class<?> _adapterClass;
+
+	private NativeAPIAdapterDelegate _delegate;
+
+	private Method _delegateMethod;
+
+	private Object _proxy;
+
+	public NativeAPIAdapterSignal(List<Slot<L>> bindings) {
+		super(bindings);
+	}
+
+	protected void init(final String adapterName, final String listenerName,
+			final Class<?>[] parameterTypes, final NativeAPIAdapterDelegate delegate,
+			final String delegateMethodName) {
+		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+		try {
+			_adapterClass = classLoader.loadClass(adapterName);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		try {
+			_delegateMethod = _delegate.getClass().getMethod(listenerName, parameterTypes);
+		} catch (SecurityException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+
+		final Class<?>[] proxyInterfaces = { _adapterClass };
+		_proxy =
+				Proxy.newProxyInstance(classLoader, proxyInterfaces, new NativeAPIAdapterProxy(
+						delegateMethodName, _delegate, _delegateMethod));
+	}
+
+	protected void removeProxy() {
+		T target = getTarget();
+		if (null != _invokeMethod) {
+			try {
+				if (!_invokeMethod.isAccessible())
+					_invokeMethod.setAccessible(true);
+
+				Object[] arguments = { null };
+				_invokeMethod.invoke(target, arguments);
+			} catch (IllegalArgumentException e) {
+				throw new RuntimeException(e);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			} catch (InvocationTargetException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		_invokeMethod = null;
+	}
+
+	protected void registerProxy() {
+		if (null != _adapterClass && null != _proxy) {
+			T target = getTarget();
+			if (null != target) {
+
+				try {
+					final Method[] methods = target.getClass().getMethods();
+					for (final Method method : methods) {
+						if (method.getName().equals(_listenerName)) {
+							_invokeMethod = method;
+
+							if (!_invokeMethod.isAccessible())
+								_invokeMethod.setAccessible(true);
+
+							Object[] arguments = { _proxy };
+							_invokeMethod.invoke(target, arguments);
+							break;
+						}
+					}
+				} catch (SecurityException e) {
+					throw new RuntimeException(e);
+				} catch (IllegalArgumentException e) {
+					throw new RuntimeException(e);
+				} catch (IllegalAccessException e) {
+					throw new RuntimeException(e);
+				} catch (InvocationTargetException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @return True if the native signal is available
+	 */
+	@Override
+	public boolean isAvailable() {
+		return null != _adapterClass && null != _delegateMethod && null != _proxy
+				&& null != _invokeMethod;
+	}
+
+	/**
+	 * Private target listener class which implements the
+	 */
+	private final class NativeAPIAdapterProxy implements InvocationHandler {
+
+		private final String _methodName;
+
+		private final NativeAPIAdapterDelegate _delegate;
+
+		private final Method _delegateMethod;
+
+		public NativeAPIAdapterProxy(String methodName, NativeAPIAdapterDelegate delegate,
+				Method delegateMethod) {
+			assert null != methodName : "Method name can not be null";
+			assert null != delegate : "Delegate can not be null";
+			assert null != delegateMethod : "Delegate method can not be null";
+
+			_methodName = methodName;
+			_delegate = delegate;
+			_delegateMethod = delegateMethod;
+		}
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
+			if (method.getName().equals(_methodName)) {
+				try {
+					if (!_delegateMethod.isAccessible())
+						_delegateMethod.setAccessible(true);
+
+					return _delegateMethod.invoke(_delegate, arguments);
+				} catch (InvocationTargetException e) {
+					throw e.getTargetException();
+				}
+			}
+
+			throw new InternalError("Unexpected method dispatched: " + method);
+		}
+	}
+
+	public static interface NativeAPIAdapterDelegate {
+
+	}
+
+	public static class NativeAPIAdapterSignal1<A> implements NativeSignal1<A> {
+
+		private final List<Slot<SignalListener1<A>>> _bindings =
+				new CopyOnWriteArrayList<Slot<SignalListener1<A>>>();
+
+		private final Dispatcher<SignalListener1<A>> _dispatcher = DispatcherImpl
+				.newInstance(_bindings);
+
+		private final NativeAPIAdapterSignal<SignalListener1<A>, A> _signal =
+				new NativeAPIAdapterSignal<SignalListener1<A>, A>(_bindings);
+
+		/**
+		 * Private constructor
+		 */
+		protected NativeAPIAdapterSignal1() {
+
+		}
+
+		/**
+		 * Create a newInstance of NativeSignal1
+		 * 
+		 * @return {@link NativeSignal1}
+		 */
+		public static <A> NativeAPIAdapterSignal1<A> newInstance() {
+			return new NativeAPIAdapterSignal1<A>();
+		}
+
+		protected void init(final String adapterName, final String listenerName,
+				final Class<?>[] parameterTypes, final NativeAPIAdapterDelegate delegate,
+				final String delegateMethodName) {
+			_signal.init(adapterName, listenerName, parameterTypes, delegate, delegateMethodName);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener1<A>> add(SignalListener1<A> listener) {
+			return _signal.add(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener1<A>> addOnce(SignalListener1<A> listener) {
+			return _signal.addOnce(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener1<A>> remove(SignalListener1<A> listener) {
+			return _signal.remove(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void removeAll() {
+			_signal.removeAll();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public int getNumListeners() {
+			return _signal.getNumListeners();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @throws IllegalAccessError
+		 *             if target is null when the dispatch method is called.
+		 */
+		public void dispatch(A value0) throws Throwable {
+			if (null == _signal.getTarget())
+				throw new IllegalAccessError("Target can not be null");
+
+			_dispatcher.dispatch(value0);
+		}
+
+		@Override
+		public A getTarget() {
+			return _signal.getTarget();
+		}
+
+		@Override
+		public void setTarget(A target) {
+			_signal.removeProxy();
+			_signal.setTarget(target);
+			_signal.registerProxy();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public boolean isAvailable() {
+			return _signal.isAvailable();
+		}
+	}
+
+	public static class NativeAPIAdapterSignal2<A, B> implements NativeSignal2<A, B> {
+
+		private final List<Slot<SignalListener2<A, B>>> _bindings =
+				new CopyOnWriteArrayList<Slot<SignalListener2<A, B>>>();
+
+		private final Dispatcher<SignalListener2<A, B>> _dispatcher = DispatcherImpl
+				.newInstance(_bindings);
+
+		private final NativeAPIAdapterSignal<SignalListener2<A, B>, A> _signal =
+				new NativeAPIAdapterSignal<SignalListener2<A, B>, A>(_bindings);
+
+		/**
+		 * Private constructor
+		 */
+		protected NativeAPIAdapterSignal2() {
+
+		}
+
+		/**
+		 * Create a newInstance of NativeSignal2
+		 * 
+		 * @return {@link NativeSignal2}
+		 */
+		public static <A, B> NativeAPIAdapterSignal2<A, B> newInstance() {
+			return new NativeAPIAdapterSignal2<A, B>();
+		}
+
+		protected void init(final String adapterName, final String listenerName,
+				final Class<?>[] parameterTypes, final NativeAPIAdapterDelegate delegate,
+				final String delegateMethodName) {
+			_signal.init(adapterName, listenerName, parameterTypes, delegate, delegateMethodName);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener2<A, B>> add(SignalListener2<A, B> listener) {
+			return _signal.add(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener2<A, B>> addOnce(SignalListener2<A, B> listener) {
+			return _signal.addOnce(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener2<A, B>> remove(SignalListener2<A, B> listener) {
+			return _signal.remove(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void removeAll() {
+			_signal.removeAll();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public int getNumListeners() {
+			return _signal.getNumListeners();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @throws IllegalAccessError
+		 *             if target is null when the dispatch method is called.
+		 */
+		public void dispatch(A value0, B value1) throws Throwable {
+			if (null == _signal.getTarget())
+				throw new IllegalAccessError("Target can not be null");
+
+			_dispatcher.dispatch(value0, value1);
+		}
+
+		@Override
+		public A getTarget() {
+			return _signal.getTarget();
+		}
+
+		@Override
+		public void setTarget(A target) {
+			_signal.removeProxy();
+			_signal.setTarget(target);
+			_signal.registerProxy();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public boolean isAvailable() {
+			return _signal.isAvailable();
+		}
+	}
+
+	public static class NativeAPIAdapterSignal3<A, B, C> implements NativeSignal3<A, B, C> {
+
+		private final List<Slot<SignalListener3<A, B, C>>> _bindings =
+				new CopyOnWriteArrayList<Slot<SignalListener3<A, B, C>>>();
+
+		private final Dispatcher<SignalListener3<A, B, C>> _dispatcher = DispatcherImpl
+				.newInstance(_bindings);
+
+		private final NativeAPIAdapterSignal<SignalListener3<A, B, C>, A> _signal =
+				new NativeAPIAdapterSignal<SignalListener3<A, B, C>, A>(_bindings);
+
+		/**
+		 * Private constructor
+		 */
+		protected NativeAPIAdapterSignal3() {
+
+		}
+
+		/**
+		 * Create a newInstance of NativeSignal3
+		 * 
+		 * @return {@link NativeSignal3}
+		 */
+		public static <A, B, C> NativeAPIAdapterSignal3<A, B, C> newInstance() {
+			return new NativeAPIAdapterSignal3<A, B, C>();
+		}
+
+		protected void init(final String adapterName, final String listenerName,
+				final Class<?>[] parameterTypes, final NativeAPIAdapterDelegate delegate,
+				final String delegateMethodName) {
+			_signal.init(adapterName, listenerName, parameterTypes, delegate, delegateMethodName);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener3<A, B, C>> add(SignalListener3<A, B, C> listener) {
+			return _signal.add(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener3<A, B, C>> addOnce(SignalListener3<A, B, C> listener) {
+			return _signal.addOnce(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener3<A, B, C>> remove(SignalListener3<A, B, C> listener) {
+			return _signal.remove(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void removeAll() {
+			_signal.removeAll();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public int getNumListeners() {
+			return _signal.getNumListeners();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @throws IllegalAccessError
+		 *             if target is null when the dispatch method is called.
+		 */
+		public void dispatch(A value0, B value1, C value2) throws Throwable {
+			if (null == _signal.getTarget())
+				throw new IllegalAccessError("Target can not be null");
+
+			_dispatcher.dispatch(value0, value1, value2);
+		}
+
+		@Override
+		public A getTarget() {
+			return _signal.getTarget();
+		}
+
+		@Override
+		public void setTarget(A target) {
+			_signal.removeProxy();
+			_signal.setTarget(target);
+			_signal.registerProxy();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public boolean isAvailable() {
+			return _signal.isAvailable();
+		}
+	}
+
+	public static class NativeAPIAdapterSignal4<A, B, C, D> implements NativeSignal4<A, B, C, D> {
+
+		private final List<Slot<SignalListener4<A, B, C, D>>> _bindings =
+				new CopyOnWriteArrayList<Slot<SignalListener4<A, B, C, D>>>();
+
+		private final Dispatcher<SignalListener4<A, B, C, D>> _dispatcher = DispatcherImpl
+				.newInstance(_bindings);
+
+		private final NativeAPIAdapterSignal<SignalListener4<A, B, C, D>, A> _signal =
+				new NativeAPIAdapterSignal<SignalListener4<A, B, C, D>, A>(_bindings);
+
+		/**
+		 * Private constructor
+		 */
+		protected NativeAPIAdapterSignal4() {
+
+		}
+
+		/**
+		 * Create a newInstance of NativeSignal4
+		 * 
+		 * @return {@link NativeSignal4}
+		 */
+		public static <A, B, C, D> NativeAPIAdapterSignal4<A, B, C, D> newInstance() {
+			return new NativeAPIAdapterSignal4<A, B, C, D>();
+		}
+
+		protected void init(final String adapterName, final String listenerName,
+				final Class<?>[] parameterTypes, final NativeAPIAdapterDelegate delegate,
+				final String delegateMethodName) {
+			_signal.init(adapterName, listenerName, parameterTypes, delegate, delegateMethodName);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener4<A, B, C, D>> add(SignalListener4<A, B, C, D> listener) {
+			return _signal.add(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener4<A, B, C, D>> addOnce(SignalListener4<A, B, C, D> listener) {
+			return _signal.addOnce(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener4<A, B, C, D>> remove(SignalListener4<A, B, C, D> listener) {
+			return _signal.remove(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void removeAll() {
+			_signal.removeAll();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public int getNumListeners() {
+			return _signal.getNumListeners();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @throws IllegalAccessError
+		 *             if target is null when the dispatch method is called.
+		 */
+		public void dispatch(A value0, B value1, C value2, D value3) throws Throwable {
+			if (null == _signal.getTarget())
+				throw new IllegalAccessError("Target can not be null");
+
+			_dispatcher.dispatch(value0, value1, value2, value3);
+		}
+
+		@Override
+		public A getTarget() {
+			return _signal.getTarget();
+		}
+
+		@Override
+		public void setTarget(A target) {
+			_signal.removeProxy();
+			_signal.setTarget(target);
+			_signal.registerProxy();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public boolean isAvailable() {
+			return _signal.isAvailable();
+		}
+	}
+
+	public static class NativeAPIAdapterSignal5<A, B, C, D, E> implements
+			NativeSignal5<A, B, C, D, E> {
+
+		private final List<Slot<SignalListener5<A, B, C, D, E>>> _bindings =
+				new CopyOnWriteArrayList<Slot<SignalListener5<A, B, C, D, E>>>();
+
+		private final Dispatcher<SignalListener5<A, B, C, D, E>> _dispatcher = DispatcherImpl
+				.newInstance(_bindings);
+
+		private final NativeAPIAdapterSignal<SignalListener5<A, B, C, D, E>, A> _signal =
+				new NativeAPIAdapterSignal<SignalListener5<A, B, C, D, E>, A>(_bindings);
+
+		/**
+		 * Private constructor
+		 */
+		protected NativeAPIAdapterSignal5() {
+
+		}
+
+		/**
+		 * Create a newInstance of NativeSignal5
+		 * 
+		 * @return {@link NativeSignal5}
+		 */
+		public static <A, B, C, D, E> NativeAPIAdapterSignal5<A, B, C, D, E> newInstance() {
+			return new NativeAPIAdapterSignal5<A, B, C, D, E>();
+		}
+
+		protected void init(final String adapterName, final String listenerName,
+				final Class<?>[] parameterTypes, final NativeAPIAdapterDelegate delegate,
+				final String delegateMethodName) {
+			_signal.init(adapterName, listenerName, parameterTypes, delegate, delegateMethodName);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener5<A, B, C, D, E>> add(SignalListener5<A, B, C, D, E> listener) {
+			return _signal.add(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener5<A, B, C, D, E>>
+				addOnce(SignalListener5<A, B, C, D, E> listener) {
+			return _signal.addOnce(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Slot<SignalListener5<A, B, C, D, E>> remove(SignalListener5<A, B, C, D, E> listener) {
+			return _signal.remove(listener);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void removeAll() {
+			_signal.removeAll();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public int getNumListeners() {
+			return _signal.getNumListeners();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @throws IllegalAccessError
+		 *             if target is null when the dispatch method is called.
+		 */
+		public void dispatch(A value0, B value1, C value2, D value3, E value4) throws Throwable {
+			if (null == _signal.getTarget())
+				throw new IllegalAccessError("Target can not be null");
+
+			_dispatcher.dispatch(value0, value1, value2, value3, value4);
+		}
+
+		@Override
+		public A getTarget() {
+			return _signal.getTarget();
+		}
+
+		@Override
+		public void setTarget(A target) {
+			_signal.removeProxy();
+			_signal.setTarget(target);
+			_signal.registerProxy();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public boolean isAvailable() {
+			return _signal.isAvailable();
+		}
+	}
+}
--- src/main/java/org/osjava/signals/natives/view/ViewOnLayoutChangeSignal.java
+++ src/main/java/org/osjava/signals/natives/view/ViewOnLayoutChangeSignal.java
@@ -1,32 +1,20 @@
 package org.osjava.signals.natives.view;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-
-import org.osjava.signals.impl.NativeSignalImpl.NativeSignalImpl5;
+import org.osjava.signals.natives.NativeAPIAdapterSignal.NativeAPIAdapterDelegate;
+import org.osjava.signals.natives.NativeAPIAdapterSignal.NativeAPIAdapterSignal5;
 
 import android.view.View;
 
 public class ViewOnLayoutChangeSignal extends
-		NativeSignalImpl5<View, Integer, Integer, Integer, Integer> {
+		NativeAPIAdapterSignal5<View, Integer, Integer, Integer, Integer> {
 
-	private final static String CLASS_NAME = "android.view.View$OnLayoutChangeListener";
+	private final static String ADAPTER_NAME = "android.view.View$OnLayoutChangeListener";
 
-	private final static String CLASS_SUBSCRIBER_NAME = "addOnLayoutChangeListener";
+	private final static String LISTENER_NAME = "addOnLayoutChangeListener";
 
-	private final static String CLASS_LISTENER_NAME = "onLayoutChange";
+	private final static String METHOD_NAME = "onLayoutChange";
 
 	private TargetListener _listener = new TargetListener();
-
-	private Method _method;
-
-	private Method _delegateMethod;
-
-	private Class<?> _interface;
-
-	private Object _proxy;
 
 	/**
 	 * Private constructor
@@ -35,7 +23,12 @@ public class ViewOnLayoutChangeSignal extends
 	 *            to be used when applying the listeners
 	 */
 	private ViewOnLayoutChangeSignal(View target) {
-		init();
+		final Class<?>[] parameterTypes =
+				{ View.class, Integer.class, Integer.class, Integer.class, Integer.class,
+						Integer.class, Integer.class, Integer.class, Integer.class };
+
+		super.init(ADAPTER_NAME, LISTENER_NAME, parameterTypes, _listener, METHOD_NAME);
+
 		setTarget(target);
 	}
 
@@ -50,153 +43,15 @@ public class ViewOnLayoutChangeSignal extends
 		return new ViewOnLayoutChangeSignal(target);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @return True if the native signal is available
-	 */
-	@Override
-	public boolean isAvailable() {
-		return null != _interface && null != _delegateMethod && null != _proxy && null != _method;
-	}
-
-	/**
-	 * Initialise method for the signal
-	 */
-	protected void init() {
-		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-
-		try {
-			_interface = classLoader.loadClass(CLASS_NAME);
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-
-		final Class<?>[] parameterTypes = { View.class, Integer.class, Integer.class,
-				Integer.class, Integer.class, Integer.class, Integer.class, Integer.class,
-				Integer.class };
-
-		try {
-			_delegateMethod = _listener.getClass().getMethod(CLASS_LISTENER_NAME, parameterTypes);
-		} catch (SecurityException e) {
-			throw new RuntimeException(e);
-		} catch (NoSuchMethodException e) {
-			throw new RuntimeException(e);
-		}
-
-		final Class<?>[] proxyInterfaces = { _interface };
-		_proxy = Proxy.newProxyInstance(classLoader, proxyInterfaces, new TargetProxyListener(
-				_listener, _delegateMethod));
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected void removeTargetListener() {
-		View view = getTarget();
-
-		if (null != view && null != _method) {
-			try {
-				if (!_method.isAccessible())
-					_method.setAccessible(true);
-
-				Object[] arguments = { null };
-				_method.invoke(view, arguments);
-			} catch (IllegalArgumentException e) {
-				throw new RuntimeException(e);
-			} catch (IllegalAccessException e) {
-				throw new RuntimeException(e);
-			} catch (InvocationTargetException e) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		_method = null;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected void registerTargetListener() {
-		if (null != _interface && null != _proxy) {
-			View view = getTarget();
-			if (null != view) {
-
-				try {
-					final Method[] methods = view.getClass().getMethods();
-					for (final Method method : methods) {
-						if (method.getName().equals(CLASS_SUBSCRIBER_NAME)) {
-							// We know the contact so we can skip the method
-							// name
-							_method = method;
-							
-							if (!_method.isAccessible())
-								_method.setAccessible(true);
-
-							Object[] arguments = { _proxy };
-							_method.invoke(view, arguments);
-							break;
-						}
-					}
-				} catch (SecurityException e) {
-					throw new RuntimeException(e);
-				} catch (IllegalArgumentException e) {
-					throw new RuntimeException(e);
-				} catch (IllegalAccessException e) {
-					throw new RuntimeException(e);
-				} catch (InvocationTargetException e) {
-					throw new RuntimeException(e);
-				}
-			}
-		}
-	}
-
-	/**
-	 * Private target listener class which implements the
-	 */
-	private final class TargetProxyListener implements InvocationHandler {
-
-		private final TargetListener _delegate;
-
-		private final Method _delegateMethod;
-
-		public TargetProxyListener(TargetListener delegate, Method delegateMethod) {
-			assert null != delegate : "TargetListener can not be null";
-			assert null != delegateMethod : "DelegateMethod can not be null";
-
-			_delegate = delegate;
-			_delegateMethod = delegateMethod;
-		}
-
-		@Override
-		public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
-			if (null != _delegateMethod) {
-				if (method.getName().equals(CLASS_LISTENER_NAME)) {
-					try {
-						if (!_delegateMethod.isAccessible())
-							_delegateMethod.setAccessible(true);
-
-						return _delegateMethod.invoke(_delegate, arguments);
-					} catch (InvocationTargetException e) {
-						throw e.getTargetException();
-					}
-				}
-			}
-
-			throw new InternalError("Unexpected method dispatched: " + method);
-		}
-	}
-
-	private final class TargetListener {
+	private final class TargetListener implements NativeAPIAdapterDelegate {
 
 		@SuppressWarnings("unused")
-		public void onLayoutChange(View view, Integer left, Integer top, Integer right,
-				Integer bottom, Integer oldLeft, Integer oldTop, Integer oldRight, Integer oldBottom) {
+		public void
+				onLayoutChange(View view, Integer left, Integer top, Integer right, Integer bottom,
+						Integer oldLeft, Integer oldTop, Integer oldRight, Integer oldBottom) {
 
 			try {
-				ViewOnLayoutChangeSignal.this.dispatch(view, left, top, right, bottom);
+				dispatch(view, left, top, right, bottom);
 			} catch (Throwable t) {
 				throw new RuntimeException(t);
 			}
