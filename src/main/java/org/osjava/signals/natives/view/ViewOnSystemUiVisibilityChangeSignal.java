38fb310a6fabaf8a136dc83a9b528141ed59e9f5
Simon Richardson <stickupkid@gmail.comm>
2011-11-15 17:45:14 +0000

    Adding OnSystemUiVisibilityChangeListener

---
+++ src/main/java/org/osjava/signals/natives/view/ViewOnSystemUiVisibilityChangeSignal.java
@@ -0,0 +1,54 @@
+package org.osjava.signals.natives.view;
+
+import org.osjava.signals.natives.NativeAPIAdapterSignal.NativeAPIAdapterDelegate;
+import org.osjava.signals.natives.NativeAPIAdapterSignal.NativeAPIAdapterSignal2;
+
+import android.view.View;
+
+public class ViewOnSystemUiVisibilityChangeSignal extends NativeAPIAdapterSignal2<View, Integer> {
+
+	private final static String ADAPTER_NAME =
+			"android.view.View$OnSystemUiVisibilityChangeListener";
+
+	private final static String LISTENER_NAME = "setOnSystemUiVisibilityChangeListener";
+
+	private final static String METHOD_NAME = "onSystemUiVisibilityChange";
+
+	/**
+	 * Private constructor
+	 * 
+	 * @param target
+	 *            to be used when applying the listeners
+	 */
+	private ViewOnSystemUiVisibilityChangeSignal(View target) {
+
+		final Class<?>[] parameterTypes = { Integer.class };
+
+		init(ADAPTER_NAME, LISTENER_NAME, parameterTypes, new TargetListener(), METHOD_NAME);
+		setTarget(target);
+	}
+
+	/**
+	 * Create a newInstance of ViewOnSystemUiVisibilityChangeSignal
+	 * 
+	 * @param View
+	 *            target to apply the listener when executing a dispatch
+	 * @return {@link ViewOnSystemUiVisibilityChangeSignal}
+	 */
+	public static ViewOnSystemUiVisibilityChangeSignal newInstance(final View target) {
+		return new ViewOnSystemUiVisibilityChangeSignal(target);
+	}
+
+	private final class TargetListener implements NativeAPIAdapterDelegate {
+
+		@SuppressWarnings("unused")
+		public void onSystemUiVisibilityChange(Integer visibility) {
+
+			try {
+				dispatch(getTarget(), visibility);
+			} catch (Throwable t) {
+				throw new RuntimeException(t);
+			}
+		}
+	}
+}
